ec68c68612563f8a7003816eef2b6aaaa00ac110
Adelino Segundo <adelinosegundo@gmail.com>
2015-10-26 23:31:13 -0300

    created small populator script

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# CleanUp
+Course.destroy_all
+User.destroy_all
+CourseUser.destroy_all
+Task.destroy_all
+
+Course.create([
+  { title: 'Topicos I' }, 
+  { title: 'Topicos II' },
+  { title: 'Topicos III' },
+  { title: 'Topicos IV' },
+  { title: 'Topicos V' },
+])
+
+10.times do |n|
+  User.create username: "user#{n}", sigaa_uuid: "0000#{n}", name: "Name #{n}"
+end
+
+User.each do |user|
+  Course.all.sample(3).each do |course|
+    CourseUser.create user: user, course: course
+  end
+end
+
+
+Course.each do |course|
+  3.times do |n|
+    Task.create status: "open", delivery_date: Time.now, title: "Some Title", description: "Some Description", attachment: "Some URL", course: course
+  end
+end
+
+
+User.all.sample(7).each do |user|
+  Task.all.sample(2).each do |task|
+    UserTask.create user: user, task: task
+  end
+end
