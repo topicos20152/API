cada346cb6a85a59ec9a256430590d180fdfd8eb
Adelino Segundo <adelinosegundo@gmail.com>
2015-10-26 23:15:32 -0300

    Create tasks API methods

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,20 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show]
+
+  # GET /tasks
+  # GET /tasks.json
+  def index
+    @tasks = Task.all
+  end
+
+  # GET /tasks/1
+  # GET /tasks/1.json
+  def show
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_task
+      @task = Task.find(params[:id])
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:update, :destroy]
 
   # POST /users
   # POST /users.json
