362fca3739853f5aea07fe4c7639de457c4dad56
Adelino Segundo <adelinosegundo@gmail.com>
2015-10-26 23:32:30 -0300

    removed html api return

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,11 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show]
 
-  # GET /tasks
   # GET /tasks.json
   def index
     @tasks = Task.all
   end
-
-  # GET /tasks/1
+  
   # GET /tasks/1.json
   def show
   end
