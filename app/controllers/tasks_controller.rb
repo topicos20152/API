35295e8943fbbaf863dce3aa9c0987ef15ece5e0
Adelino Segundo <adelinosegundo@gmail.com>
2015-10-28 09:16:32 -0300

    forcing json routes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show]
+  before_filter :set_default_response_format
 
   # GET /tasks.json
   def index
@@ -14,5 +15,9 @@ class TasksController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_task
       @task = Task.find(params[:id])
+    end
+
+    def set_default_response_format
+      request.format = :json
     end
 end
