cbc32da36eda0aa35011e9f04ab6266d532120c3
Adelino Segundo <adelinosegundo@gmail.com>
2015-11-18 17:25:24 -0300

    hotfix to tokken algorithm

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::UsersController < Api::V1::BaseController
   before_action :set_user
+  before_filter :check_token, except: :request_access_token
+
 
   # POST /users/1
   def request_access_token
@@ -12,7 +14,7 @@ class Api::V1::UsersController < Api::V1::BaseController
   # GET /users/1/tasks
 
   def tasks
-    @tasks = @user.tasks
+    @tasks = @user.tasks(params[:q])
     render 'api/v1/tasks/index'
   end
 
@@ -21,6 +23,13 @@ class Api::V1::UsersController < Api::V1::BaseController
       @user = User.find(params[:id])
     end
 
+    def check_token
+      access_token = params[:access_token]
+      unless access_token and @user.check_access_token(access_token)
+        render json: "Invalid access token", status: :unauthorized
+      end
+    end
+    
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       params[:user]
